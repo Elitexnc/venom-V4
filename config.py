938c8dfec606ded2027f5efd25d0bb592dfd94f6
Elitexnc <animeciv@gmail.com>
2024-08-02 11:06:13 +0530

    config.py

--- config.py
+++ config.py
@@ -2,13 +2,13 @@
 #SCRIPT BY VENOMxCRAZY
   
 #Enter Your Bot Token here get it from @botfarher
-BOT_TOKEN=("7424479555:AAG21En_vWhghzaUAhKjJ-DoZaLV3KBH4")
+BOT_TOKEN=("6957481433:AAG43GLB2PTw-zbxFV8h8NllbjM40X2ErkI")
 
   #Enter Your telegram username here without @
-OWNER_USERNAME=("venomXcrazy")
+OWNER_USERNAME=("Elitenc")
 
   #Enter your admin id here Get it from @missRose_bot by typing /info
-ADMIN_IDS=("5588464519")
+ADMIN_IDS=("1505981671")
 
 
 
